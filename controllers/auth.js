7b64244c3d7a81be43b0bc9f4454fda2953b6f84
Vipul <dowhile03@gmail.com>
2020-09-12 23:12:36 +0530

    first

---
+++ controllers/auth.js
@@ -0,0 +1,59 @@
+const { resolveInclude } = require('ejs');
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5f59b1e3b93c2c07e0994b86')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({email: email}).then(userDoc => {
+    if(userDoc) {
+      return res.redirect('/signup');
+    }
+    const user = new User({
+      email: email,
+      password: password,
+      cart: { item: []}
+    })
+    return user.save()
+  }).then(result => {
+    res.redirect('/login')
+  }).catch(err => {
+    console.log(err)
+  });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
